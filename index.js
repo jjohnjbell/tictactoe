7b3303ad94cedd44f6e04f840375f75fe66b4822
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2021-11-29 14:44:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,6 @@
 // Waits for the window document to load before running javascript
 window.onload = function () {
-    /*
-        Code Review - Michael Cowan
-        NOTE: Comments are strictly to explain the thought process. Regularly code wouldn't be written like this.
-        NOTE: I also chaneded the CSS and HTML files to match new ID's and variable names.
-
-
-        Issue 0: While some variable names are obvious in context, they aren't declarative without context.
-        Solution: Use declarative variable names or appropriate shortenings, shortening variable names !== better/faster code.
-        (As long as they don't become sentences).
-        Action(s): 
-            - use industry standard shortenings for common elements. Example: "button" => "btn" or "table" => "tbl".
-            - using the full name for elements. Example: "mess" => "message".
-            - specify if the variable actually stores the element or element value. Example: "message" => "messageElement".
-        
-        Issue 1: A lot of repetition is done throughout the code statically, meaning the code isn't maintainable or scalable.
-        Solution: Use a data structure like an array or list to save similar elements/values and loop over them instead.
-        Action(s): 
-            - create arrays for groups of relative variables/elements. Example: The "winConditions" array.
-            - use for-loops or array-loops to run similar operations multiple times.
-  
-        Issue 2: Save computation time.
-        Solution: Look for more opportunities for if-statements. Don't run code if it doesn't have to. 
-        Action(s):
-            - there's no reason to check if someone won if they were only 4 turns. 5 turns are needed minimum to win a game.
-            - let the player know the game ended when the last possible move or all 9 spots have been entered.
-            - 
-  
-        Issue 3: Possible layout shifting.
-        Solution: I didn't test it, but changing the font size of the elements shouldn't be necessary if you just set the innerText to an empty string.
-    */
-
-    // const but1 = document.getElementById("a1")
-    // const but2 = document.getElementById("a2")
-    // const but3 = document.getElementById("a3")
-    // const but4 = document.getElementById("a4")
-    // const but5 = document.getElementById("a5")
-    // const but6 = document.getElementById("a6")
-    // const but7 = document.getElementById("a7")
-    // const but8 = document.getElementById("a8")
-    // const but9 = document.getElementById("a9")
-
+   
     //Grab graphic elements
     const googleEl = `<img src="images/google.jpg" height="100" width="100">`
     const chromeEl = `<img src="images/chrome.jpg" height="100" width="100">`
@@ -59,18 +19,7 @@ window.onload = function () {
     const messageElement = document.getElementById("message");
     const newButtonElement = document.getElementById("reset-btn");
 
-    /* 
-        Since buttons are stored in an array now, we'll use the array indexes to identify buttons to win conditions.
-        (Instead of their variable names, which no longer exist)
-        Image the array relative to the tic-tac-toe grid as such:
-        
-         0 | 1 | 2  
-        ---+---+---
-         3 | 4 | 5   
-        ---+---+---
-         6 | 7 | 8  
-    */
-
+  
     const winConditions = [
         // Horizontal Conditions
         [0, 1, 2],
@@ -90,22 +39,7 @@ window.onload = function () {
     let player = "X"; // Keep track of current player
     let turnsPlayed = 0; // Keep track of total turns played
     resetGame();
-    // clearBoard();
-
-    // function renderGame(butt) {
-    //     if (butt.innerText != "") {
-    //         if (player1Turn) {
-    //             butt.innerText = "X"
-    //             butt.style.fontSize = "40px"
-    //             player1Turn = false
-    //         } else {
-    //             butt.innerText = "O"
-    //             butt.style.fontSize = "40px"
-    //             player1Turn = true
-    //         }
-    //     }
-    //     checkWinner()
-    // }
+  
 
     function handleButtonClick(button) {
         // Only run function if button is empty AND the game hasn't already ended.
@@ -132,24 +66,6 @@ window.onload = function () {
     
     }
 
-    // function checkWinner() {
-    //     if (((but1.textContent === but2.textContent) && (but2.textContent === but3.textContent)) ||
-    //         ((but4.textContent === but5.textContent) && (but5.textContent === but6.textContent)) ||
-    //         ((but7.textContent === but8.textContent) && (but8.textContent === but9.textContent)) ||
-
-    //         ((but1.textContent === but4.textContent) && (but4.textContent === but7.textContent)) ||
-    //         ((but2.textContent === but5.textContent) && (but5.textContent === but8.textContent)) ||
-    //         ((but3.textContent === but6.textContent) && (but6.textContent === but9.textContent)) ||
-
-    //         ((but1.textContent === but5.textContent) && (but5.textContent === but9.textContent)) ||
-    //         ((but3.textContent === but5.textContent) && (but5.textContent === but7.textContent))) {
-
-    //         gameEnd = true
-    //         message.textContent = "Congratulations"
-    //     } else {
-    //         gameEnd = false
-    //     }
-    // }
 
     function checkWinCondition() {
         // Get the last player to make a move, so we can check if they won the game.
@@ -194,65 +110,8 @@ window.onload = function () {
             }
         }
     }
+    }
 
-    // but1.addEventListener("click", function () {
-    //     renderGame(but1)
-    // })
-    // but2.addEventListener("click", function () {
-    //     renderGame(but2)
-    // })
-    // but3.addEventListener("click", function () {
-    //     renderGame(but3)
-    // })
-    // but4.addEventListener("click", function () {
-    //     renderGame(but4)
-    // })
-    // but5.addEventListener("click", function () {
-    //     renderGame(but5)
-    // })
-    // but6.addEventListener("click", function () {
-    //     renderGame(but6)
-    // })
-    // but7.addEventListener("click", function () {
-    //     renderGame(but7)
-    // })
-    // but8.addEventListener("click", function () {
-    //     renderGame(but8)
-    // })
-    // but9.addEventListener("click", function () {
-    //     renderGame(but9)
-    // })
-
-    // function clearBoard() {
-    //     mess.textContent = ""
-
-    //     but1.textContent = "z"
-    //     but1.style.fontSize = "0px"
-
-    //     but2.textContent = "q"
-    //     but2.style.fontSize = "0px"
-
-    //     but3.textContent = "w"
-    //     but3.style.fontSize = "0px"
-
-    //     but4.textContent = "e"
-    //     but4.style.fontSize = "0px"
-
-    //     but5.textContent = "r"
-    //     but5.style.fontSize = "0px"
-
-    //     but6.textContent = "t"
-    //     but6.style.fontSize = "0px"
-
-    //     but7.textContent = "l"
-    //     but7.style.fontSize = "0px"
-
-    //     but8.textContent = "k"
-    //     but8.style.fontSize = "0px"
-
-    //     but9.textContent = "t"
-    //     but9.style.fontSize = "0px"
-    // }
 
     // Call the function resetGame instead of clearBoard because it does more than just clearning the board, it resets the game state as well.
     function resetGame() {
@@ -270,9 +129,15 @@ window.onload = function () {
         }
     }
 
+
+
+
+
     newButtonElement.addEventListener("click", function () {
         gameEnd = false;
         player1Turn = true;
         resetGame();
     });
-}; // Close window onload function
+
+
+} // Close window onload function
